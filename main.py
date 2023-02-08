55d7852e138b4fe8dfefcb32237b241ca82c91d4
fomichev <fomkirich@gmail.com>
2023-02-08 19:39:30 +0500

    Z_4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import flask
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
@@ -6,24 +7,37 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html', title='Заготовка')
+    return render_template('index.html', title='заготовка')
 
 
 @app.route('/training/<prof>')
 def training(prof):
-    return render_template('training.html', prof=prof)
+    return render_template('training.html', title=prof)
 
 
-@app.route('/list/<prof>')
+@app.route('/list_prof/<list>')
 def list_prof(list):
-    prof = ('инженер-исследователь', 'пилот', 'строитель',
-            'экзобиолог', 'врач', 'инженер по терраформированию',
-            'климатолог', 'специалист по радиационной защите',
-            'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
-            'метеоролог', 'оператор марсохода', 'киберинженер',
-            'штурман', 'пилот дронов')
+    prof = (
+        'инженер-исследователь', 'пилот', "строитель", 'экзобиолог', 'врач', 'инженер по терраформированию',
+        'климатолог',
+        'пециалист по радиационной защите', 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения', 'метеоролог',
+        'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов')
     return render_template('list_prof.html', list=list, prof=prof)
 
 
-if __name__ == '__main__':
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    pasrams = {'title': 'Анкета',
+               "surname": "Сусанин",
+               "name": "Иван",
+               "education": "4 класса",
+               "profession": "экскурсовод",
+               "sex": "м",
+               "motivation": "всегда мечтал застрять на Марсе",
+               'ready': 'всегда готов'}
+    return render_template('auto_answer.html', **pasrams)
+
+
+if __name__ == "__main__":
     app.run()
